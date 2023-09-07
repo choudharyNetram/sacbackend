ebd37b6f75d3a8a272bdb835ef6c0350cadf2f78
choudharyNetram <101733146+choudharyNetram@users.noreply.github.com>
2023-09-07 18:48:18 +0530

    Update server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,20 @@
 module.exports = ({ env }) => ({
+  // ...other config settings
+  upload: {
+    provider: 'local',
+    providerOptions: {
+      // Adjust this path to match your directory structure
+      public: './public/uploads',
+    },
+  },
+  // ...other config settings
+});
+
+
+/*module.exports = ({ env }) => ({
     url: env("RENDER_EXTERNAL_URL"),
     dirs: {
       public: "/public"
     },
   });
+  */
