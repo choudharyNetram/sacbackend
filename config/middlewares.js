1d54db8735783b12357b79fe7ad02a7e5e645596
choudharyNetram <choudharynetram7@gmail.com>
2023-09-07 19:50:17 +0530

    ready to deply

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,30 @@
-module.exports = [
+/*module.exports = [
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
+
   'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
+*/
+
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:1337', 'https://aciitgnbackend.onrender.com/', ]
+    }
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
